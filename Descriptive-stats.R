6cf702ba00b6a51d912e2ffdccb46a7b7d3b2423
Bimal Itani <116113697+bimalitani100@users.noreply.github.com>
2025-09-01 22:01:17 -0400

    Add files via upload

---
+++ Descriptive-stats.R
@@ -0,0 +1,17 @@
+# Author: Bimal_Itani, Date: 2025 Aug 28th, Purpose: Measure of Descriptive Stats
+
+
+# Generate some sample datasets
+
+data <- c(1:50)
+
+# Calaulate mean/average for the data 
+mean(data)
+
+# Calculate median for the data 
+median(data)
+
+#Calculate Standard Deviation
+sd(data)
+
+
